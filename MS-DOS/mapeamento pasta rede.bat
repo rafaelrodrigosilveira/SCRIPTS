0e079da0a6588291d54de3c3a9b62d09e86e1220
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-04-18 16:09:51 -0300

    v0.9.2
    correção script compartilhamento pasta em rede windows

--- MS-DOS/mapeamento pasta rede.bat
+++ MS-DOS/mapeamento pasta rede.bat
@@ -1,1 +1,29 @@
-net use g: \\servidor\pasta
+REM EXPLICAÇAO:
+
+REM K: = Unidade a ser criada no compartilhamento.
+
+REM SERVERNAME = Nome ou IP da máquina q vc vai acessar.
+
+REM C$ = Unidade compartilhada na máquina server.
+
+REM SENHA = senha do usuário na máquina server.
+
+REM USUARIO = usuário devidamente cadastrado na máquina server.
+
+REM /PERSISTENT:YES = SALVA CONFIGURAÇOES APOS LOGON
+
+REM %username% = todos usuarios
+
+
+REM PARA USUARIO ESPECIFICO:
+
+
+
+REM NET USE K: \\SERVERNAME\C$ SENHA /USER:USUARIO /PERSISTENT:YES
+
+
+
+
+REM PARA USUARIO ATUAL:
+
+NET USE K: \\192.168.1.1\volume1 /USER:%username% /PERSISTENT:YES
